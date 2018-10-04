b7d1d86a81cf9c215ae507b7288908f085e3aa66
MOHD ABDUL <RAHEEM.MOHDABDUL@thomsonreuters.com>
2018-10-04 19:55:49 +0530

    Add project files.

---
+++ Even_Odd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Even_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+            
+            
+                if (a % 2 == 1)
+                {
+                    Console.WriteLine(a + "is odd number");
+                }
+                else
+                    Console.WriteLine(a + " is Even Number ");
+            }
+            catch (Exception ) {
+                Console.WriteLine("Please Enter a valid input\n");
+            }
+            }
+        
+            }
+        }
+          
